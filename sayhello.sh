ab63a562de34dad9c534448b5af96271169af8c3
Owen Shepherd <414owen@gmail.com>
2016-10-28 12:38:45 +0100

    Edited verbosity

--- sayhello.sh
+++ sayhello.sh
@@ -9,12 +9,11 @@ while true; do
 	newlogins=$(diff /tmp/lastloggedon /tmp/loggedon | grep -E "^>" | sed "s/^> //g")
 	for person in ${people}; do
 		user="$(echo "${person}" | sed "s/ .*//g")"
-		echo "Checking if ${user} is logged on"
 		for loggedonuser in ${newlogins}; do
-			echo "checking against ${loggedonuser}"
 			if [ "${loggedonuser}" = "$user" ]; then
 				message="$(echo "${person}" | sed -E 's/.*"(.*)"/\1/g')"
-				echo "found ${user}, sending this:\n${message}"
+				echo "Found new user ${user}, sending this:"
+				echo "${message}"
 				echo "${message}" | write "${loggedonuser}" 
 			fi
 		done
