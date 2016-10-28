e028058ef0f0a2736a353e2615fb90035b98dcf8
Owen Shepherd <414owen@gmail.com>
2016-10-28 12:29:03 +0100

    Added diffing, only pings newly logged on people

--- sayhello.sh
+++ sayhello.sh
@@ -2,21 +2,25 @@
 
 IFS=$'\n'
 people=$(cat users)
+touch /tmp/lastloggedon
 
 while true; do
-	loggedon="$(ps aux | grep pts | grep sshd | grep -v "grep" | sed "s/ .*//g")"
-	for person in $people; do
-		user="$(echo "$person" | sed "s/ .*//g")"
-		echo "Checking if $user is logged on"
-		#echo "$message"
-		for loggedonuser in $loggedon; do
-			echo "checking against $loggedonuser"
-			if [ "$loggedonuser" = "$user" ]; then
-				message="$(echo "$person" | sed -E 's/.*"(.*)"/\1/g')"
+	ps aux | grep pts | grep sshd | grep -v "grep" | sed "s/ .*//g" > /tmp/loggedon
+	newlogins=$(diff /tmp/lastloggedon /tmp/loggedon | grep -E "^>" | sed "s/^> //g")
+	for person in ${people}; do
+		user="$(echo "${person}" | sed "s/ .*//g")"
+		echo "Checking if ${user} is logged on"
+		for loggedonuser in ${newlogins}; do
+			echo "checking against ${loggedonuser}"
+			if [ "${loggedonuser}" = "$user" ]; then
+				message="$(echo "${person}" | sed -E 's/.*"(.*)"/\1/g')"
 				echo "found ${user}, sending this:\n${message}"
-				echo "$message" | write "$loggedonuser" 
+				echo "${message}" | write "${loggedonuser}" 
 			fi
 		done
 	done
+	rm /tmp/lastloggedon
+	mv /tmp/loggedon /tmp/lastloggedon
+	lastloggedon=${loggedon}
 	sleep 1
 done
